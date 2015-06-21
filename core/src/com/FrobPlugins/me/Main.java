7194b52a8aac61901ee6563c937a3538becd8d48
Robbiecool2000 <robbie3000vanderheijden@gmail.com>
2015-06-21 11:40:32 +0200

    Test 2

--- core/src/com/FrobPlugins/me/Main.java
+++ core/src/com/FrobPlugins/me/Main.java
@@ -12,7 +12,7 @@ public class Main extends ApplicationAdapter {
 	
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("assets/badlogic.jpg");
 	}
 
 	public void render () {
