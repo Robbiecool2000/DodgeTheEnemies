40f2c0044320320c73b4bd54ac6c71b5b28018c2
Robbiecool2000 <robbie3000vanderheijden@gmail.com>
2015-07-16 22:00:39 +0200

    Started creating level 2

--- core/src/com/FrobPlugins/me/Level2.java
+++ core/src/com/FrobPlugins/me/Level2.java
@@ -17,10 +17,12 @@ public class Level2 extends Game implements Screen{
 	//Texture
 	Texture Character;
 	Texture Level2Background;
+	Texture EvilCharacter;
 	
 	//Sprites
 	Sprite sprite_character;
 	Sprite sprite_level2background;
+	Sprite sprite_evilcharacter;
 	
 	//Font
 	public static BitmapFont font;
@@ -29,6 +31,11 @@ public class Level2 extends Game implements Screen{
 	
 	//(Evil)CharBounds
 	private final Rectangle CharBounds;
+	private final Rectangle EvilCharBounds_1;
+	private final Rectangle EvilCharBounds_2;
+	
+	int EvilCharAtTop_1 = 0;
+	int EvilCharAtTop_2 = 0;
 	
 	private OrthographicCamera camera;
 	Main main;
@@ -38,6 +45,8 @@ public class Level2 extends Game implements Screen{
 		LoadTexture();
 		LoadSprite();
 		CharBounds = new Rectangle(360, 160, 60, 60);
+		EvilCharBounds_1 = new Rectangle(0, 0, 80, 80);
+		EvilCharBounds_2 = new Rectangle(720, 0, 80, 80);
 	}
 	public void create(){
 		
@@ -58,10 +67,16 @@ public class Level2 extends Game implements Screen{
 		CharControls();
 		CharBoundaries();
 		Setupfont();
+		EvilCharIntSystem();
+		EvilCharMovement();
+		EvilCharBackMovement();
+		Collide();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 			batch.draw(sprite_level2background, 0, 0);
 			batch.draw(sprite_character, CharBounds.x, CharBounds.y);
+			batch.draw(sprite_evilcharacter, EvilCharBounds_1.x, EvilCharBounds_1.y);
+			batch.draw(sprite_evilcharacter, EvilCharBounds_2.x, EvilCharBounds_2.y);
 		batch.end();
 	}
 	public void resize(int arg0, int arg1) {
@@ -80,12 +95,13 @@ public class Level2 extends Game implements Screen{
 	public void LoadTexture(){
 		Character = new Texture("assets/DodgeTheEnemiesCharacter.png");
 		Level2Background = new Texture("assets/LevelBackground.png");
+		EvilCharacter = new Texture("assets/DodgeTheEnemiesEvilCharacter.png");
 	}
 	
 	public void LoadSprite(){
 		sprite_character = new Sprite(Character);
 		sprite_level2background = new Sprite(Level2Background);
-		
+		sprite_evilcharacter = new Sprite(EvilCharacter);
 	}
 	
 	public void CharControls(){
@@ -119,5 +135,49 @@ public class Level2 extends Game implements Screen{
 	}
 	public void Setupfont(){
 		font = new BitmapFont(Gdx.files.internal("assets/Font/MyFont.fnt"));
+	}
+	public void EvilCharIntSystem(){
+		if(EvilCharBounds_1.y == 0){
+			EvilCharAtTop_1 = 0;
+		}
+		if(EvilCharBounds_1.y == 320){
+			EvilCharAtTop_1 = 1;
+		}
+		
+		if(EvilCharBounds_2.y == 0){
+			EvilCharAtTop_2 = 0;
+		}
+		if(EvilCharBounds_2.y == 320){
+			EvilCharAtTop_2 = 1;
+		}
+	}
+	public void EvilCharMovement(){
+		if(EvilCharAtTop_1 == 0){
+			EvilCharBounds_1.y += 1;
+			EvilCharBounds_1.x += 2.25;
+		}
+		
+		if(EvilCharAtTop_2 == 0){
+			EvilCharBounds_2.y += 1;
+			EvilCharBounds_2.x -= 2.25;
+		}
+	}
+	public void EvilCharBackMovement(){
+		if(EvilCharAtTop_1 == 1){
+			EvilCharBounds_1.y -= 1;
+			EvilCharBounds_1.x -= 2.25;
+		}
+		if(EvilCharAtTop_2 == 1){
+			EvilCharBounds_2.y -= 1;
+			EvilCharBounds_2.x += 2.25;
+		}
+	}
+	public void Collide(){
+		if(CharBounds.overlaps(EvilCharBounds_1)){
+			System.out.println("You died!");
+		}
+		if(CharBounds.overlaps(EvilCharBounds_2)){
+			System.out.println("You died!");
+		}
 	}
 }
