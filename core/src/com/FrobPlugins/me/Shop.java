a27237aeb42b46ec422dcaa0ab6cbfee4290fa43
Robbiecool2000 <robbie3000vanderheijden@gmail.com>
2015-08-11 14:49:28 +0200

    Added backbutton in shop

--- core/src/com/FrobPlugins/me/Shop.java
+++ core/src/com/FrobPlugins/me/Shop.java
@@ -1,20 +1,40 @@
 package com.FrobPlugins.me;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class Shop implements Screen{
+	
+	private Stage button_stage = new Stage();
+	private TextureAtlas atlas;
+	private TextButton back_button;
+	private Skin skin;
+	private Table table;
 	
 	//Textures
 	Texture ShopBackground;
 	
 	//Sprites
 	Sprite sprite_shopbackground;
+	
+	//Font
+	public static BitmapFont font;
 	
 	Main main;
 	
@@ -48,6 +68,8 @@ public class Shop implements Screen{
 		batch.begin();
 			batch.draw(sprite_shopbackground, 0, 0);
 		batch.end();
+		button_stage.act();
+		button_stage.draw();
 	}
 
 	public void resize(int arg0, int arg1) {
@@ -67,11 +89,45 @@ public class Shop implements Screen{
 		camera.update();
 		LoadTexture();
 		LoadSprite();
+		Setupfont();
+		
+		Gdx.input.setInputProcessor(button_stage);
+		
+		atlas = new TextureAtlas("assets/button.pack");
+		skin = new Skin(atlas);
+		
+		table = new Table(skin);
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		TextButtonStyle textButtonStyle = new TextButtonStyle(); 
+		textButtonStyle.up = skin.getDrawable("button_up");
+		textButtonStyle.down = skin.getDrawable("button_down");
+		textButtonStyle.pressedOffsetX = 1;
+		textButtonStyle.pressedOffsetY = -1;
+		textButtonStyle.font = font;
+		
+		back_button = new TextButton("Back", textButtonStyle);
+		back_button.pad(10);
+		back_button.addListener(new ClickListener(){
+			public void clicked(InputEvent event, float x, float y){
+				((Game)Gdx.app.getApplicationListener()).setScreen(new MainMenu(main));
+			}
+		});
+		
+		table.add(back_button);
+		table.getCell(back_button).spaceBottom(10);
+		table.getCell(back_button).prefSize(100, 50);
+		table.getCell(back_button).pad(Main.SCREEN_HEIGHT/2 + 150, Main.SCREEN_WIDTH/2 + 300, 0, 0);
+		button_stage.addActor(table);
 	}
 	public void LoadTexture(){
 		ShopBackground = new Texture("assets/Background.png");
 	}
 	public void LoadSprite(){
 		sprite_shopbackground = new Sprite(ShopBackground);
+	}
+	public void Setupfont(){
+		font = new BitmapFont(Gdx.files.internal("assets/data/arial-15.fnt"));
+		font.setColor(Color.BLACK);
 	}
 }
