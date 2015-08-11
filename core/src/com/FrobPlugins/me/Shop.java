13d76548ed41071c3ab26dbe397140f1e271eb7e
Robbiecool2000 <robbie3000vanderheijden@gmail.com>
2015-08-11 11:41:53 +0200

    Added the Shop screen

--- core/src/com/FrobPlugins/me/Shop.java
+++ core/src/com/FrobPlugins/me/Shop.java
@@ -1,56 +1,77 @@
 package com.FrobPlugins.me;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Shop implements Screen{
-
+	
+	//Textures
+	Texture ShopBackground;
+	
+	//Sprites
+	Sprite sprite_shopbackground;
 	
 	Main main;
+	
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
 	
 	public Shop(Main main){
 		this.main = main;
 	}
 	
-	@Override
 	public void dispose() {
 		
 		
 	}
-
-	@Override
+	
 	public void hide() {
 		
 		
 	}
-
-	@Override
+	
 	public void pause() {
 		
 		
 	}
 
-	@Override
-	public void render(float arg0) {
-		
-		
+	public void render(float deltaTime) {
+		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+			batch.draw(sprite_shopbackground, 0, 0);
+		batch.end();
 	}
 
-	@Override
 	public void resize(int arg0, int arg1) {
 		
 		
 	}
 
-	@Override
 	public void resume() {
 		
 		
 	}
 
-	@Override
 	public void show() {
-		
-		
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 400);
+		camera.update();
+		LoadTexture();
+		LoadSprite();
 	}
-
+	public void LoadTexture(){
+		ShopBackground = new Texture("assets/Background.png");
+	}
+	public void LoadSprite(){
+		sprite_shopbackground = new Sprite(ShopBackground);
+	}
 }
