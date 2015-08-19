e78733fa0a4ffd7cde67820c901b721da60fdc6d
Robbiecool2000 <robbie3000vanderheijden@gmail.com>
2015-08-19 14:12:07 +0200

    Removed unused imports

--- core/src/com/FrobPlugins/me/MainMenu.java
+++ core/src/com/FrobPlugins/me/MainMenu.java
@@ -12,13 +12,11 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.sun.javafx.property.adapter.PropertyDescriptor.Listener;
 
 public class MainMenu extends Game implements Screen {
 	public static OrthographicCamera camera;
